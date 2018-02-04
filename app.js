29f3b3628ebe1a0b3cd34563d26da60070daf3bd
SIYUAN HU <siyuanhu@SIYUANs-MBP.gateway>
2018-02-04 22:48:37 +1100

    finished the script

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ var roundScore = 0;
 var activePlayer = 0;
 // the dice number
 var dice = 1;
-var diceImage = document.getElementsByClassName("dice");
+var diceImage = document.querySelector(".dice");
+var p1CurrentNumber = document.querySelector("#current-0");
+var p2CurrentNumber = document.querySelector("#current-1");
+var isPlaying = false;
+
 
 //generate a random number from 1 to the max number
 function generateDiceNumber(){
@@ -26,47 +30,107 @@ function generateDiceNumber(){
 }
 
 function rollDice(){
+    
+    isPlaying = true;
     dice = generateDiceNumber();
+
     //display a dice in the screen based on the number
-    console.log(dice);
+  
     switch (dice){
-        case 1: diceImage.src = 'dice-1.png';break;
-        case 2: diceImage.src = 'dice-2.png';break;
-        case 3: diceImage.src = 'dice-3.png';break;
-        case 4: diceImage.src = 'dice-4.png';break;
-        case 5: diceImage.src = 'dice-5.png';break;
-        case 6: diceImage.src = 'dice-6.png';break;
+        case 1: diceImage.src = "dice-1.png";break;
+        case 2: diceImage.src = "dice-2.png";break;
+        case 3: diceImage.src = "dice-3.png";break;
+        case 4: diceImage.src = "dice-4.png";break;
+        case 5: diceImage.src = "dice-5.png";break;
+        case 6: diceImage.src = "dice-6.png";break;
             
+    } 
+    diceImage.style.display = 'inline';
+    if(dice === 1){
+        resetTotalNumber();
+        changePlayer();
+    }else{
+         //roundscore will be added by the new dice number
+        roundScore = roundScore + dice;
+        //change the current number of the active player
+        getCurrentNumberLabel().textContent = roundScore;   
     }
-    //roundscore will be added by the new dice number
-    roundScore = roundScore+dice;
+   
+}
+
+function getCurrentNumberLabel(){
+    var currentNumberLabel = document.querySelector("#current-"+activePlayer);
+    return currentNumberLabel;
+}
+
+function getCurrentScoreLabel(){
+     var currentScoreLabel = document.querySelector("#score-"+activePlayer);
+    return currentScoreLabel;
+}
+
+function resetTotalNumber(){ 
+        roundScore = 0;
+         scores[activePlayer] = 0;
+         getCurrentScoreLabel().textContent = scores[activePlayer]; 
+        getCurrentNumberLabel().textContent = roundScore;
     
 }
 
-function hold(){
-  // the total score of the activeplayer will be added by the current roundscore, the roundscroe will become 0    
-  scores[activePlayer] = scores[activePlayer] + roundScore;
-  roundScore = 0;    
-}
-
-function startGame(){
-    //initialise the current number and total number of players on the webpage
+function changePlayer(){
     
-    
-    
-    //如果click roll dice
-    rollDice();
-    
-    //如果click hold
-    hold();
-    
-    if(dice===1){
         if(activePlayer === 0){
             activePlayer = 1;
         }else{
              activePlayer = 0;
         }
-        
-    }
+        document.querySelector('.player-0-panel').classList.toggle("active");
+     document.querySelector('.player-1-panel').classList.toggle("active");
+    
+         diceImage.style.display = 'None';
     
 }
+
+function setTotalNumber(){
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    getCurrentScoreLabel().textContent = scores[activePlayer]; 
+}
+
+function hold(){
+  // the total score of the activeplayer will be added by the current roundscore, the roundscroe will become 0 
+  if(isPlaying){
+      setTotalNumber();
+      roundScore = 0;  
+      if(scores[activePlayer]>=100){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+                document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            isPlaying=false;
+      }else{
+          getCurrentNumberLabel().textContent = 0;
+          changePlayer();
+
+      }
+  }
+}
+
+
+
+
+document.querySelector(".btn-new").addEventListener('click',init);
+document.querySelector(".btn-roll").addEventListener('click',rollDice);
+document.querySelector(".btn-hold").addEventListener("click",hold);
+
+
+function init(){
+     //initialise the current number and total number of players on the webpage
+   
+    p1CurrentNumber.textContent = 0;
+    p2CurrentNumber.textContent = 0;
+    var p1Score = document.querySelector("#score-0").textContent = 0;
+    var p2Score = document.querySelector("#score-1").textContent = 0;
+    //hide the dice image
+    diceImage.style.display = 'None';
+}
+
+
